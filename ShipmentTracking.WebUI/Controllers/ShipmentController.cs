4c48c8b50d34a999c86e7e57645d0af5b301d903
bugrabalim11 <bugrablm11@gmail.com>
2026-06-17 22:31:20 +0300

    feat: WebUI (MVC) projesi oluşturuldu, HttpClient entegre edildi ve kargo listeleme sayfası API'ye bağlandı.

---
+++ ShipmentTracking.WebUI/Controllers/ShipmentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShipmentTracking.Entities.DTOs.Shipment;
+using System.Text.Json.Serialization;
+
+namespace ShipmentTracking.WebUI.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        // API ile konuşmamızı sağlayacak sanal tarayıcı nesnemiz
+        private readonly HttpClient _httpClient;
+
+        public ShipmentController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // 1. Kargo Listeleme Sayfası (Index)
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7204/api/Shipments");
+            if (response.IsSuccessStatusCode)
+            {
+                // API'den gelen ham metni (JSON) oku
+                var jsonString = await response.Content.ReadAsStringAsync();
+
+                // O metni, C# listesine (List<ShipmentListDto>) dönüştür (Sihirli Kısım)
+                var shipments = JsonConvert.DeserializeObject<List<ShipmentListDto>>(jsonString);
+
+                // Verileri View'a (Arayüze) gönder
+                return View(shipments);
+            }
+            return View(new List<ShipmentListDto>());
+        }
+    }
+}
---
+++ ShipmentTracking.WebUI/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+// Sisteme MVC yeteneklerini ekler
+builder.Services.AddControllersWithViews();
+
+// ---> EKLENECEK SİHİRLİ SATIR <---
+// Sisteme HttpClient (Sanal Tarayıcı) yeteneğini öğretir
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Shipment}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
