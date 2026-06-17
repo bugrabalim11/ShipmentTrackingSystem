654b4808a368610c8150e028c24bfaa156b7cf8a
bugrabalim11 <bugrablm11@gmail.com>
2026-06-17 09:11:12 +0300

    feat: kargo hareket geçmişi için API uçları oluşturuldu.
    - ShipmentHistoriesController sisteme dahil edildi.
    - GET ve POST metotları yazıldı.
    - Swagger üzerinden kargo geçmişi ekleme (201) ve listeleme testleri başarıyla yapıldı.
    - API katmanının temel Controller altyapısı %100 tamamlandı.

---
+++ ShipmentTracking.API/Controllers/ShipmentHistoriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShipmentTracking.Business.Abstract;
+using ShipmentTracking.Entities.Concrete;
+
+namespace ShipmentTracking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipmentHistoriesController : ControllerBase
+    {
+        private readonly IShipmentHistoryService _shipmentHistoryService;
+
+        // Dependency Injection ile Servisimizi içeri alıyoruz
+        public ShipmentHistoriesController(IShipmentHistoryService shipmentHistoryService)
+        {
+            _shipmentHistoryService = shipmentHistoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result=await _shipmentHistoryService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ShipmentHistory shipmentHistory)
+        {
+            await _shipmentHistoryService.AddAsync(shipmentHistory);
+            return StatusCode(201);
+        }
+    }
+}
